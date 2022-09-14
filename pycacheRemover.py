e3e0600be8006b18f5f86735c924fab6598728b1
ctolon <cevat.batuhan.tolon@cern.ch>
2022-09-15 00:22:41 +0300

    fix pycache remover

--- pycacheRemover.py
+++ pycacheRemover.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 # Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 # See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
@@ -17,16 +17,29 @@
 
 import os
 
+
 class PycacheRemover(object):
     def __init__(self):
-        
+
         super(PycacheRemover, self).__init__()
 
-        commandOne = 'python3 -Bc ' + "\"" + "import pathlib;" + "[p.unlink() for p in pathlib.Path('.').rglob('*.py[co]')]" + "\""
-        commandTwo = 'python3 -Bc'  + "\"" + "import pathlib;" + "[p.rmdir() for p in pathlib.Path('.').rglob('__pycache__')]" + "\""
+        commandOne = (
+            "python3 -Bc "
+            + '"'
+            + "import pathlib;"
+            + "[p.unlink() for p in pathlib.Path('.').rglob('*.py[co]')]"
+            + '"'
+        )
+        commandTwo = (
+            "python3 -Bc"
+            + '"'
+            + "import pathlib;"
+            + "[p.rmdir() for p in pathlib.Path('.').rglob('__pycache__')]"
+            + '"'
+        )
 
         os.system(commandOne)
         os.system(commandTwo)
-        
-        #print(commandOne)
-        #print(commandTwo)
+
+        # print(commandOne)
+        # print(commandTwo)
