5cb5e15d1d81645ad4d73947c71b5c6d04e1e696
ctolon <cevat.batuhan.tolon@cern.ch>
2022-12-31 05:52:49 +0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,8 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
+extensions = ['sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages', 'sphinx_rtd_theme']
+# extensions = ['sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -34,6 +35,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
 html_static_path = ['_static']
