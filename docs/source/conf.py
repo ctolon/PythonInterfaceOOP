9ee01e07913e419f1b3bf17a40438f7d6e82c0c2
ctolon <cevat.batuhan.tolon@cern.ch>
2022-12-31 06:36:06 +0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 import os
 import sys
 sys.path.insert(1, os.path.abspath('..'))
+sys.path.insert(1, os.path.abspath('../..'))
 sys.path.insert(1, os.path.abspath('../../PythonInterfaceOOP/'))
 sys.path.insert(1, os.path.abspath('../../PythonInterfaceOOP/extramodules/'))
 
