f908be0d832c17fe268aec09a7fcb529afe6be78
ctolon <cevat.batuhan.tolon@cern.ch>
2022-12-31 05:44:40 +0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
